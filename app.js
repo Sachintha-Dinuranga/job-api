3c20e3596aa7326d86e560a62cf3329f21644d1d
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2026-07-12 12:43:39 +0530

    Add unhandle route error handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import jobs from "./routes/jobs.js";
 import { connectDB } from "./config/database.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
+import ErrorHandler from "./utils/errorHandler.js";
 
 // setting up env file
 dotenv.config();
@@ -25,11 +26,17 @@ connectDB();
 // define routes
 app.use("/api/v1", jobs);
 
+// Handle unhandled routes
+app.all(/.*/, (req, res, next) => {
+  next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+});
+
 // Middleware to handle errors
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
+// Setting up the server
 const server = app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
