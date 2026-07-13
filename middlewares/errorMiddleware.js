4bdea8c91885105a446d1079482c07d24f51d68f
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2026-07-13 11:10:30 +0530

    Add wrong mongoose error handler and mongoose validation error handler

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,3 +1,5 @@
+import ErrorHandler from "../utils/errorHandler";
+
 export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
@@ -15,6 +17,18 @@ export default (err, req, res, next) => {
     let error = { ...err };
 
     error.message = err.message;
+
+    // wrong mongoose object id error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid ${err.path}`;
+      error = new ErrorHandler(message, 404);
+    }
+
+    // handling mongoose validation errors
+    if (err.name === "ValidationError") {
+      const message = Obaject.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
 
     res.status(err.statusCode).json({
       success: false,
